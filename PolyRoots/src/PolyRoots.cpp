44d84301d72d511d62099c91585f411d804e0bef
BilboB47 <Fabian@DESKTOP-DAR3TOU>
2025-09-14 15:44:21 +0200

    nazwewnictwo

--- PolyRoots/src/PolyRoots.cpp
+++ PolyRoots/src/PolyRoots.cpp
@@ -4,7 +4,7 @@
 #include <iostream>
 #include <cmath>
 
-std::vector<double> make_derivative_from_poly(const std::vector<double>& coeffs) {
+std::vector<double> derivativeFromPoly(const std::vector<double>& coeffs) {
 
 	std::vector<double> derivative; 
 	const int degree = coeffs.size()-1;
@@ -16,7 +16,7 @@ std::vector<double> make_derivative_from_poly(const std::vector<double>& coeffs)
 	return derivative;
 }
 
-std::string make_poly_from_vector(const std::vector<double>& coeffs) {
+std::string polyFromVector(const std::vector<double>& coeffs) {
 	const int degree = coeffs.size();
 	if (degree < 1)return "";
 
@@ -36,13 +36,13 @@ std::string make_poly_from_vector(const std::vector<double>& coeffs) {
 	return poly;
 }
 
-double point_zero_from_linear(const std::vector<double>& coeffs){
+double zeroFromLinear(const std::vector<double>& coeffs){
 	if (coeffs.size() > 2)throw std::invalid_argument("Vector does not represent a linear polynomial OR none zero point");
 
 	return coeffs[1] * (-1) / coeffs[0];
 }
 
-double value_of_poly_at_x(const std::vector<double>& coeffs,const double& x) {
+double valueAtX(const std::vector<double>& coeffs,const double& x) {
 	const int size = coeffs.size(); //3 x^(2) +  5x^(1) - 4     
 
 	double y = coeffs[0];
@@ -53,13 +53,13 @@ double value_of_poly_at_x(const std::vector<double>& coeffs,const double& x) {
 	return y;
 }
 
-Point extrema_from_linear(const std::vector<double>& coeffs) {
+Point rootExtremumType(const std::vector<double>& coeffs) {
 
 	if (coeffs.size() != 2)throw std::invalid_argument("Vector does not represent a linear polynomial");
 	
-	double p = point_zero_from_linear(coeffs);
+	double p = zeroFromLinear(coeffs);
 
-	if (value_of_poly_at_x(coeffs, p - PRECISION) < 0) {
+	if (valueAtX(coeffs, p - PRECISION) < 0) {
 		return Point(p,MINIMUM);
 	}
 	else {
@@ -68,11 +68,18 @@ Point extrema_from_linear(const std::vector<double>& coeffs) {
 
 }
 
-double bisection_extrema_between_2_points(const  std::vector<double>& coeffs, double left, double right) {
+Point rootExtremumType(const std::vector<double>& coeffs, const double x) {
+	if (std::fabs(valueAtX(coeffs, x)) > PRECISION * 10)throw std::invalid_argument("x is not zero point");
+
+	if (valueAtX(coeffs, x - PRECISION) > 0)return Point(x, MAXIMUM);
+	return Point(x, MINIMUM);
+}
+
+double bisection(const  std::vector<double>& coeffs, double left, double right) {
 	if(left>right) throw std::invalid_argument("Wrong interval");
 
-	double value_left = value_of_poly_at_x(coeffs, left);
-	double value_right = value_of_poly_at_x(coeffs, right);
+	double value_left = valueAtX(coeffs, left);
+	double value_right = valueAtX(coeffs, right);
 
 	if(value_left < 0 && value_right < 0 || value_left > 0 && value_right > 0)throw std::invalid_argument("Not 1 zero point");
 	
@@ -81,7 +88,7 @@ double bisection_extrema_between_2_points(const  std::vector<double>& coeffs, do
 	while (std::fabs(left - right) > PRECISION) {
 
 		double midle = (left + right) / 2.0;
-		double value_midle = value_of_poly_at_x(coeffs, midle);
+		double value_midle = valueAtX(coeffs, midle);
 
 		if (value_midle > 0) {
 			if (left_positive) left = midle;
@@ -97,10 +104,4 @@ double bisection_extrema_between_2_points(const  std::vector<double>& coeffs, do
 	return (left + right) / 2.0;
 }
 
-Point extrema_from_zero(const std::vector<double>& coeffs,const double x){
-	if(std::fabs(value_of_poly_at_x(coeffs,x)) > PRECISION*10)throw std::invalid_argument("x is not zero point");
-
-	if (value_of_poly_at_x(coeffs, x - PRECISION) > 0)return Point(x,MAXIMUM);
-	return Point(x, MINIMUM);
-}
 
--- PolyRoots/src/PolyRoots.h
+++ PolyRoots/src/PolyRoots.h
@@ -10,17 +10,19 @@ struct Point {
 	Point(double x_val, State st) : x(x_val), state(st) {};
 };
 
-std::string make_poly_from_vector(const std::vector<double>& coeffs);
+std::string polyFromVector(const std::vector<double>& coeffs);
 
-std::vector<double> make_derivative_from_poly(const std::vector<double>& coeffs);
+std::vector<double> derivativeFromPoly(const std::vector<double>& coeffs);
 
-double point_zero_from_linear(const std::vector<double>& coeffs);
+double zeroFromLinear(const std::vector<double>& coeffs);
 
-double value_of_poly_at_x(const std::vector<double>& coeffs, const double& x);  
+double valueAtX(const std::vector<double>& coeffs, const double& x);  
 
-Point extrema_from_linear(const std::vector<double>& coeffs);
+Point rootExtremumType(const std::vector<double>& coeffs);
 
-double bisection_extrema_between_2_points(const std::vector<double>& coeffs,double left, double right);
+Point rootExtremumType(const std::vector<double>& coeffs, const double x);
 
-Point extrema_from_zero(const std::vector<double>& coeffs,const double x);
+double bisection(const std::vector<double>& coeffs,double left, double right);
+
+
 
